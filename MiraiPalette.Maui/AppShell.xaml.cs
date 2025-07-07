75ba8a6d4d1e53db57339bb3ead64d6755954ac9
Tianchen Zhou <remeamiku@gmail.com>
2025-07-07 16:08:45 +0900

    サイドバーの幅とタイトルを変更
    AppShell.xamlでFlyoutWidthを200から55に変更し、ShellContentのタイトルを「Home」から「Palette」に変更しました。アイコンも「Home48」から「Color24」に変更されています。OptionsのShellContentはアイコンを「Options48」のままにし、内容の構造を調整しました。
    
    AppShell.xaml.csでは、サイドバーの初期状態を折りたたまれている（false）に変更し、OnSideBarButtonClickedメソッドの引数名を変更しました。アニメーションの持続時間を200ミリ秒から250ミリ秒に変更し、サイドバーの展開と折りたたみのアニメーションを調整しました。
    
    PaletteDetailPage.xamlでは、Grid要素にMarginとPaddingを追加し、レイアウトを改善しました。

--- MiraiPalette.Maui/AppShell.xaml.cs
+++ MiraiPalette.Maui/AppShell.xaml.cs
@@ -6,16 +6,17 @@ namespace MiraiPalette.Maui
         public AppShell()
         {
             InitializeComponent();
+            OnSideBarButtonClicked(default!, default!);
         }
 
-        private bool _isSideBarExpanded = true;
+        private bool _isSideBarExpanded = false;
 
-        private void OnSideBarButtonClicked(object sender, EventArgs e)
+        private void OnSideBarButtonClicked(object _, EventArgs e)
         {
             if(_isSideBarExpanded)            
-               new Animation(w => FlyoutWidth = w,200,55,Easing.Default).Commit(this, "FoldSideBar", length:200);            
+               new Animation(w => FlyoutWidth = w,250,55,Easing.Default).Commit(this, "FoldSideBar", length:200);            
             else            
-                new Animation(w => FlyoutWidth = w, 55, 200, Easing.Default).Commit(this, "ExpandSideBar", length: 200);            
+                new Animation(w => FlyoutWidth = w, 55, 250, Easing.Default).Commit(this, "ExpandSideBar", length: 200);            
             _isSideBarExpanded = !_isSideBarExpanded;
         }
     }
